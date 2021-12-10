560d6d8f9faa15e718971e2ebb118d2d489b0874
Kim Hyunjae <loma1016@gmail.com>
2021-12-10 23:29:32 +0900

    material ui setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
-import './App.css';
+import Header from './components/Header/Header';
+import styled from 'styled-components';
 
-function App() {
-  return <div>ㅇ</div>;
-}
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import GlobalStyle from './styles/GlobalStyle';
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: #fff0f0;
+`;
+
+const App: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Container>
+        <GlobalStyle />
+        <Header />
+      </Container>
+    </BrowserRouter>
+  );
+};
 
 export default App;
