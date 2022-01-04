beaa5636e815422469c9608e67d65ebddda4d8a2
Kim Hyunjae <loma1016@gmail.com>
2022-01-04 21:39:25 +0900

    post
    post test

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import LoginInput from './LoginInput/LoginInput';
+import axios from 'axios';
 
 const Container = styled.div`
   display: flex;
@@ -11,8 +12,17 @@ const Container = styled.div`
 `;
 
 const LoginForm: React.FC = () => {
-  const onSubmit = (form: { email: string; password: string }) => {
+  const onSubmit = async (form: { email: string; password: string }) => {
     console.log(form);
+    await axios
+      .post('http://localhost:8282/colorPalette/login/loginCheck', form)
+      .then((r) => {
+        console.log(r);
+        console.log(`${form} 폼 정상적으로 작동`);
+      })
+      .catch((res) => {
+        console.error('err');
+      });
   };
 
   return (
